bcc609ec85d25ca4749f99dca67f83af9f25b340
Guilherme <gfbatista1@gmail.com>
2024-09-17 22:08:02 -0300

    criado o component de visualizar reserva e o esqueleto estatico da tela

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './authentication/register/register.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { TesteComponent } from './teste/teste.component';
+import { ViewBookingComponent } from './client/view-booking/view-booking.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,10 @@ export const routes: Routes = [
     {
         path: 't',
         component: TesteComponent
+    },
+    {
+        path: 'view-booking',
+        component: ViewBookingComponent
     }
+
 ];
---
+++ src/app/client/view-booking/view-booking.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewBookingComponent } from './view-booking.component';
+
+describe('ViewBookingComponent', () => {
+  let component: ViewBookingComponent;
+  let fixture: ComponentFixture<ViewBookingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ViewBookingComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ViewBookingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

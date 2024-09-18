0ff7a31c1f62c118a662409ec86943ec046f410d
Vinicius Ratzke Servelo <vinicius_servelo@hotmail.com>
2024-09-18 18:35:55 -0300

    criado o component e o esqueleto estatico da tela

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './authentication/register/register.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { TesteComponent } from './teste/teste.component';
+import { HomeScreenComponent } from './client/home-screen/home-screen.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 't',
         component: TesteComponent
+    },
+    {
+        path: 'home',
+        component: HomeScreenComponent
     }
 ];
---
+++ src/app/client/home-screen/home-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-screen.component.html',
+  styleUrl: './home-screen.component.css'
+})
+export class HomeScreenComponent {
+
+}

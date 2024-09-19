d3705a4600c48710f05600e0adc45428a3a5aab5
stromae <claudio.oliveira10.0p@gmail.com>
2024-09-19 14:58:05 -0300

    create models Airport and Flight

---
+++ src/shared/models/Flight.model.ts
@@ -0,0 +1,11 @@
+export class Flight {
+    cod?: string;
+    dateDeparture?: string; // data/hora
+    departureAirport?: string;
+    destinationAirport?: string;
+    price?: number;
+    totalSeats?: number;
+    occupiedSeats?: number
+
+
+}

d772f668be11ee11124ca4c0906680e9b3993cf5
Maferibe <40439226+CapitaoFelix@users.noreply.github.com>
2024-09-25 17:51:34 -0300

    listagem funcionario
    listagem funcionario

---
+++ src/app/users/employee/list-employee/list-employee.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../layout/navbar/navbar.component";
+import { Flight } from '../../../../shared/models/Flight.model';
+import { CommonModule, NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-list-employee',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    NgFor,
+    CommonModule
+  ],
+  templateUrl: './list-employee.component.html',
+  styleUrl: './list-employee.component.css'
+});

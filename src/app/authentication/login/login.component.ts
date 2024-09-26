28997a8308befbac570a2a05b86a35b3295fa144
stromae <claudio.oliveira10.0p@gmail.com>
2024-09-26 15:34:19 -0300

    create screen and service

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,14 +1,17 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Authentication } from '../../../shared/models/Authentication.model';
+import { Authentication, AuthenticationType } from '../../../shared/models/Authentication.model';
 import { NgIf } from '@angular/common';
+import { LoginService } from './services/login.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     FormsModule,
-    NgIf
+    NgIf,
+    RouterLink
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -18,5 +21,25 @@ export class LoginComponent {
 
   auth: Authentication = new Authentication()
 
+  constructor( private loginService: LoginService, private router: Router ) {}
+
   public errorMessage = ''
+
+  async authentication() {
+    const result = await this.loginService.login(this.auth.login, this.auth.password)
+    this.handleLogin(result)
+  }
+
+  handleLogin(result: any) {
+    const roleUser: number = result.data.login.roleUser
+    if (roleUser == AuthenticationType.EMPLOYEE) {
+      console.log("it is a employee")
+      this.router.navigate(['/eh'])
+    } else if (roleUser === AuthenticationType.CUSTOMER) {
+      console.log("it is a customer")
+      this.router.navigate(['/home'])
+    } else {
+      console.log("who is this dude??")
+    }
+  }
 }

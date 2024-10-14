b17f6f8d49edd714b2db1708c314fc2f332a3293
Mateus Irineu Guimarães Mallmann <mateusmallmann2013@gmail.com>
2024-10-14 08:10:41 -0300

    Fix template listar funcionario
    Fix template listar funcionario

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Authentication} from '../../../shared/models/Authentication.model';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  //imports: [CommonModule],
   imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
 export class NavbarComponent {
+
+  //public roleUser: number | undefined;
+  //public roleUser:number = 2;
+
+  //constructor(private auth: Authentication) {}
+
+  ngOnInit(): void {
+    //this.roleUser = this.auth.role;
+  }
 
 }

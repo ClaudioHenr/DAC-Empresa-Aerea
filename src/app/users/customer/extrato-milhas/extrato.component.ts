eac5b2c897d4c00e01453aac20329d2fc0cb0a79
Guilherme <gfbatista1@gmail.com>
2024-10-31 16:29:29 -0300

    ajustes integração extrato gateway

--- src/app/users/customer/extrato-milhas/extrato.component.ts
+++ src/app/users/customer/extrato-milhas/extrato.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NavbarCustomerComponent } from '../../../layout/navbar-customer/navbar-customer.component';
-import { MilesService } from '../../../services/miles.services';
+import { MilesService } from '../../../services/miles.service';
 
 @Component({
   selector: 'app-extrato-milhas',
